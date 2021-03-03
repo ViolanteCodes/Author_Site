3806a483750692156cee58656ef2e0c716ba3caa
ViolanteCodes <maria@mviolante.com>
2021-03-03 10:40:37 -0500

    added a model for jumbotron landing page and started bootstrap creation of landing page

--- home/models.py
+++ home/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from django.conf import settings
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+    ]
+
+class LandingPage(Page):
+    """A stylish, custom landing page with jumbotron and dual images"""
+    jumbotron_background = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
+    jumbotron_left_image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
+    jumbotron_right_image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
+    jumbotron_text = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('jumbotron_background'),
+        ImageChooserPanel('jumbotron_left_image'),
+        ImageChooserPanel('jumbotron_right_image'),
+        FieldPanel('jumbotron_text', classname="full"),
     ]
