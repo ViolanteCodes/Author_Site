bc83d38c2c12ac7e72500601a9dcf0fe23a0d696
ViolanteCodes <maria@mviolante.com>
2021-03-07 13:56:55 -0500

    reclassing homepage

--- home/models.py
+++ home/models.py
@@ -6,25 +6,22 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from django.conf import settings
 
 
+# class HomePage(Page):
+#     body = RichTextField(blank=True)
+#     content_panels = Page.content_panels + [
+#         FieldPanel('body', classname="full"),
+#     ]
+
 class HomePage(Page):
-    body = RichTextField(blank=True)
-    content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
-    ]
-
-class LandingPage(Page):
     """A stylish, custom landing page with jumbotron and dual images"""
-    jumbotron_background = models.ForeignKey(
+    background_image = models.ForeignKey(
         'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
-    jumbotron_left_image = models.ForeignKey(
+    book_image = models.ForeignKey(
         'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
-    jumbotron_right_image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+', null=True)
-    jumbotron_text = RichTextField(blank=True)
+    caption_text = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        ImageChooserPanel('jumbotron_background'),
-        ImageChooserPanel('jumbotron_left_image'),
-        ImageChooserPanel('jumbotron_right_image'),
-        FieldPanel('jumbotron_text', classname="full"),
+        ImageChooserPanel('background_image'),
+        ImageChooserPanel('book_image'),
+        FieldPanel('caption_text', classname="full"),
     ]
