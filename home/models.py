5910544e1cc4db90f60b57d654a747cb71565c99
ViolanteCodes <maria@mviolante.com>
2021-03-07 14:30:35 -0500

    removed multi-field panel from landing page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from django.conf import settings
 
