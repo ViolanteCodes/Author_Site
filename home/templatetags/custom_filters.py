2e8053b55b96204304772b54fbf93b144fdd74a1
ViolanteCodes <maria@mviolante.com>
2021-03-07 23:03:02 -0500

    changed coloring and breakpoints on books template

---
+++ home/templatetags/custom_filters.py
@@ -0,0 +1,13 @@
+# These next two lines are necessary for Django to recognize that 
+# the following function calls are for custom filters.
+
+from django import template
+from django.conf import settings
+register = template.Library()
+
+# Custom filters go here.
+@register.simple_tag
+def get_site_name():
+    """Simple tag to pull site name from settings."""
+    site_name = settings.WAGTAIL_SITE_NAME
+    return site_name
